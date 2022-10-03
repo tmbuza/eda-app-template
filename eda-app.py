8c9d423e36ff4486d4a8ba283a951b844b7dc49a
tmbuza <ndelly@gmail.com>
2022-10-03 15:12:07 -0500

    Update eda-app.py

--- eda-app.py
+++ eda-app.py
@@ -19,7 +19,8 @@ st.set_page_config(
      initial_sidebar_state='expanded')
      
 from plotnine.data import diamonds
-diamonds.to_csv("data/diamonds.csv") 
+diamonds = diamonds.iloc[:, 0:]
+diamonds.to_csv("data/preprocessed_diamonds.csv", index = False) 
 
 # App Title
 with st.container():
@@ -39,32 +40,28 @@ with st.container():
       """)
       
   with panel2:
-    st.warning(
+    st.info(
       """
       #### Users must:
       - Upload input file using the user\'s widget on the left sidebar. 
-      - Make sure that the uploaded data is tidy. See the input data structure [here]().
+      - Make sure that the uploaded data is tidy.
       """
     )
 
 with st.sidebar.header('User input widget'):
-    uploaded_file = st.sidebar.file_uploader("Please choose a CSV file to explore", type=["csv"])
-    
-    st.sidebar.markdown("""
-    
-    """)
-    
-# [Example CSV input file](https://raw.githubusercontent.com/tmbuza/my-app-template/main/data/iris.csv)
+    uploaded_file = st.sidebar.file_uploader("Please choose a tidy CSV file", type=["csv"])
+
 if uploaded_file is None:
-    st.info(':exclamation: Awaiting user\'s input file')
+    st.warning(':exclamation: Awaiting user\'s input file')
     
-    if st.button('Run a Demo Data, shows minimal data exploration'):
+    if st.button('Run a Demo Data'):
       # Using a demo data
       @st.cache
       def load_data():
-          a = pd.read_csv('data/iris.csv')
+          a = pd.read_csv('data/preprocessed_diamonds.csv')
           return a
       df = load_data()
+      st.write(""" > This demo uses a preprocessed `diamond dataset` for demonstration only.""")
       st.header("""Demo Data Exploration""")
       st.subheader("""Input DataFrame""")
       st.write("Head", df.head())
